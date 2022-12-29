bece1850d1e38e7240a02e86285cf84899e66141
BijayKumarNayak <91744852+BijayKumarNayak@users.noreply.github.com>
2022-12-29 09:53:51 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+let progress = document.querySelector("#progress");
+let song = document.querySelector("#song");
+let ctrlIcon = document.querySelector("#ctrlIcon");
+
+song.onloademetada = function () {
+  progress.max = song.duration;
+  progress.value = song.currentTime;
+};
+function playpause() {
+  if (ctrlIcon.classList.contains("fa-pause")) {
+    song.pause();
+
+    ctrlIcon.classList.remove("fa-pause");
+    ctrlIcon.classList.add("fa-play");
+  } else {
+    song.play();
+
+    ctrlIcon.classList.add("fa-pause");
+    ctrlIcon.classList.remove("fa-play");
+    setInterval(() => {
+      progress.value = song.currentTime;
+    }, 1000);
+  }
+}
+progress.onchange = function () {
+  song.play();
+  console.log("play");
+  song.currentTime = progress.value;
+  ctrlIcon.classList.add("fa-pause");
+  ctrlIcon.classList.remove("fa-play");
+};
